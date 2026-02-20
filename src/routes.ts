c2387f5b51407b4ff97627fc3463ab63ff05f0c4
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-02-20 17:38:10 +0100

    Refactor route configuration and update paths

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,20 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 
-console.log("Routes config loading...");
+
 
 export default [
-  index("./dashboard.tsx"),
-  
-    route("home","./components/home.tsx"),
-    route("formation","./components/formation.tsx"),
-    route("offredemploie","./components/offredemploie.tsx"),
-    route("offredestage","./components/offredestage.tsx"),
-    route("DeveloppeurFullStack","./components/DeveloppeurFullStack.tsx"),
-    route("DataEngenieur","./components/DataEngenieur.tsx"),
-    route("Excel","./components/Excel.tsx"),
-    route("WebDesigner","./components/WebDesigner.tsx"),
-    route("MarketingDigital","./components/MarketingDigital.tsx"),
+
+    
+  index("components/home.tsx"),
+    route("/DataEngenieur", "components/DataEngenieur.tsx"),
+    route("/DeveloppeurFullStack", "components/DeveloppeurFullStack.tsx"),
+    route("/Excel", "components/Excel.tsx"),
+    route("/formation", "components/formation.tsx"),
+    route("/MarketingDigital", "components/MarketingDigital.tsx"),
+    route("/offredemploie", "components/offredemploie.tsx"),
+    route("/offredestage", "components/offredestage.tsx"),
+    route("/WebDesigner", "components/WebDesigner.tsx"),
 
 
-
-
-
-  route("about", "./about.tsx"),
-  // route("dashboard", "./dashboard.tsx"),
-  route("*?", "./catchall.tsx"),
 ] satisfies RouteConfig;
-   
